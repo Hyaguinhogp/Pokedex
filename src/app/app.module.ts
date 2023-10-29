0939a0dff695f99512afeeedf65fce2269de75cc
Hyaguinhogp <hyago.v.a.m@gmail.com>
2023-10-28 20:40:28 -0400

    feat: header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HomePresentationComponent } from './components/home-presentation/home-presentation.component';
+import { HomePokemonsComponent } from './components/home-pokemons/home-pokemons.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    HomePresentationComponent,
+    HomePokemonsComponent
   ],
   imports: [
     BrowserModule,
