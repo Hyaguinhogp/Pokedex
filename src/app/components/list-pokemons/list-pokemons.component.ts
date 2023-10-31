4c4117852ce4d8c582aa31a52d4c1d15bad62105
Hyaguinhogp <hyago.v.a.m@gmail.com>
2023-10-30 22:16:44 -0400

    feat: list pokemons responsivity

--- src/app/components/list-pokemons/list-pokemons.component.ts
+++ src/app/components/list-pokemons/list-pokemons.component.ts
@@ -7,4 +7,13 @@ import { Component, Input } from '@angular/core';
 })
 export class ListPokemonsComponent {
   @Input() pokemonList: any;
+  @Input() mobileColumns: 3 | 2 = 3;
+  @Input() desktopColumns: 4 | 6 = 6;
+
+  classes: string[] = ['pokemons-table'];
+
+  ngOnInit() {
+    this.classes.push(this.mobileColumns == 2 ? 'two-columns' : 'three-columns');
+    this.classes.push(this.desktopColumns == 4 ? 'four-columns' : 'six-columns');
+  }
 }
--- src/app/pages/pokemon/pokemon.component.ts
+++ src/app/pages/pokemon/pokemon.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
+import { LoadingService } from 'src/app/services/loading.service';
 
 @Component({
   selector: 'app-pokemon',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./pokemon.component.css']
 })
 export class PokemonComponent {
+  pokemons: any;
 
+  constructor(
+    private api: ApiService,
+    private loadingService: LoadingService
+  ) {}
+
+  ngOnInit() {
+    this.api.listWithLimit(18)
+      .subscribe((data: any) => {
+        this.pokemons = data.results;
+        this.loadingService.hide();
+      })
+  }
 }
